a256360a133397b29356e9e61d31d6bb327a1672
nathan <phs960418@gmail.com>
2021-07-24 11:37:50 +0900

    collect static

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -10,10 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_devs'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # BASE_DIR/static
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,3 +134,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
